ed6bdd5c0f061c917c557798b64c749213dc8c23
Kuniwak <orga.chem.job@gmail.com>
2018-01-12 12:21:09 +0900

    Make test readable

--- Tests/DelegateKitTests/DelegateSeedTests.swift
+++ Tests/DelegateKitTests/DelegateSeedTests.swift
@@ -5,22 +5,10 @@ import DelegateKit
 
 
 class DelegateSeedTests: XCTestCase {
-    func testExample1() {
-        let spy = DelegateUnit1Spy()
+    func testExample() {
+        let spy = DelegateSeedSpy()
 
-        let holder = DelegateUnit1Holder()
-        holder.delegates.append(spy.asAny())
-
-        holder.notifyToDelegates()
-
-        XCTAssertEqual(spy.callArgs, [.didCall])
-    }
-
-
-    func testExample2() {
-        let spy = DelegateUnit2Spy()
-
-        let holder = DelegateUnit2Holder()
+        let holder = DelegateSeedHolder()
         holder.delegates.append(spy.asAny())
 
         holder.notifyToDelegates()
@@ -30,10 +18,10 @@ class DelegateSeedTests: XCTestCase {
 
 
     func testMemoryLeak() {
-        var spy: DelegateUnit1Spy? = DelegateUnit1Spy()
+        var spy: DelegateSeedSpy? = DelegateSeedSpy()
         let weakSpy = spy!.asAny()
 
-        let holder = DelegateUnit1Holder()
+        let holder = DelegateSeedHolder()
         holder.delegates.append(weakSpy)
 
         spy = nil
@@ -42,14 +30,13 @@ class DelegateSeedTests: XCTestCase {
 
 
     static var allTests = [
-        ("testExample1", testExample1),
-        ("testExample2", testExample2),
+        ("testExample", testExample),
     ]
 }
 
 
 
-class DelegateUnit1Spy: DelegateSeed {
+class DelegateSeedSpy: DelegateSeed {
     // This code means that the delegate can take a Void parameter.
     typealias P = Void
 
@@ -69,41 +56,9 @@ class DelegateUnit1Spy: DelegateSeed {
 
 
 
-class DelegateUnit2Spy: DelegateSeed {
-    typealias P = Void
-
-
-    private(set) var callArgs: [CallArgs] = []
-
-
-    func didCall(_ parameters: Void) {
-        self.callArgs.append(.didCall)
-    }
-
-
-    enum CallArgs {
-        case didCall
-    }
-}
-
-
-
-class DelegateUnit1Holder {
-    // This is a declaration for the delegate. It means the delegate is a DelegateUnit1 and the delegate have
+class DelegateSeedHolder {
+    // This is a declaration for the delegate. It means the delegate is a DelegateSeed1 and the delegate have
     // no type parameters and the delegate take no parameters.
-    var delegates: [AnyDelegate<Void>] = []
-
-
-    func notifyToDelegates() {
-        for delegate in self.delegates {
-            delegate.didCall(())
-        }
-    }
-}
-
-
-
-class DelegateUnit2Holder {
     var delegates: [AnyDelegate<Void>] = []
 
 
